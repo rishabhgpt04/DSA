57e29fc956275ae63fcaad45a918310e6bf68eae
Rishabh Gupta <42600435+rishabhgpt04@users.noreply.github.com>
2026-07-07 22:29:02 -0400

    Time: 74 ms (7.46%) | Memory: 48.9 MB (46.39%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,15 +5,24 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        first=head
-        def helper(head):
-            nonlocal first
-            if head == None or head.next ==None:
-                return (head,True)
-            last , chk= helper(head.next)
-            if last.val != first.val:
-                chk = False
-            first = first.next 
-            return (head,chk)
-        _,res=helper(head)
-        return res
+        def reverse(head):
+            if head == None or head.next == None :
+                return head
+            newHead = reverse(head.next)
+            head.next.next = head
+            head.next = None
+            return newHead
+        slow = head
+        fast=head
+        while(fast and fast.next):
+            slow = slow.next
+            fast=fast.next.next
+        rev_head=reverse(slow)
+        print(head.val)
+        print(rev_head.val)
+        while(rev_head):
+            if head.val!=rev_head.val:
+                return False
+            head=head.next
+            rev_head=rev_head.next
+        return True
