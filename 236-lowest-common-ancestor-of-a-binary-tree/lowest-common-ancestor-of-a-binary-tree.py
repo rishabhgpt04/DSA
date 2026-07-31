e68080674de8114a28adbed4fdbc9000354c7463
Rishabh Gupta <42600435+rishabhgpt04@users.noreply.github.com>
2026-07-31 18:43:39 -0400

    Time: 143 ms (24.28%) | Memory: 50.8 MB (40.68%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.py
@@ -7,26 +7,19 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        
-        def dfs(root,par,mp,mp_val_node):
-            if root == None:
-                return
-            if root.val not in mp_val_node:
-                mp_val_node[root.val]=root  
-            mp[root.val]=par
-            dfs(root.left,root.val,mp,mp_val_node) 
-            dfs(root.right,root.val,mp,mp_val_node)
-        mp={}
-        mp_val_node={}
-        dfs(root,float('inf'),mp,mp_val_node)
-        
-        temp1 = p.val
-        temp2= q.val
-        ancesstors= set()
-        while(temp1!=float('inf')):
-            ancesstors.add(temp1)
-            temp1=mp[temp1]
-        while temp2 not in ancesstors:
-            temp2=mp[temp2]
-        return mp_val_node[temp2] 
-            
+        # without space 
+        def dfs(root,p,q):
+            if root == None :
+                return None
+            if root == p or root == q:
+                return root
+            lans = dfs(root.left,p,q)
+            rans = dfs(root.right,p,q)
+            if lans and rans:
+                return root
+            if lans:
+                return lans
+            if rans:
+                return rans
+            return None
+        return dfs(root,p,q)
